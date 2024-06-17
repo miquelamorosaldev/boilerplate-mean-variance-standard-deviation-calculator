63525c713897cd0a5d96dd0588ca6d6153c44f13
Miquel Amorós <mamoro10@xtec.cat>
2024-06-17 18:06:52 +0000

    OK Solution from Miquel Amorós

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,20 @@
 import numpy as np
 
-def calculate(list):
+def calculate(pylist):
+    # arr should contain 9 digits
+    if len(pylist) < 9:
+        raise ValueError ('List must contain nine numbers.')
+    else:
+        arr = np.array(pylist)
 
-
-
-
+    # arr is reshaped to a 3 by 3 matrix.
+    stats_array = arr.reshape((3,3))
+    calculations = {
+        'mean': [list(stats_array.mean(axis=0)), list(stats_array.mean(axis=1)), stats_array.mean()],
+        'variance': [list(stats_array.var(axis=0)), list(stats_array.var(axis=1)), stats_array.var()],
+        'standard deviation': [list(stats_array.std(axis=0)), list(stats_array.std(axis=1)), stats_array.std()],
+        'max': [list(stats_array.max(axis=0)), list(stats_array.max(axis=1)), stats_array.max()],
+        'min': [list(stats_array.min(axis=0)), list(stats_array.min(axis=1)), stats_array.min()],
+        'sum': [list(stats_array.sum(axis=0)), list(stats_array.sum(axis=1)), stats_array.sum()],
+    }
     return calculations
